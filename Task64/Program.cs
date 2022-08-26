6c8c93001952b8fbd662b805599c99437f62c046
BogdanKorolev <bkorolevjob@gmail.com>
2022-08-26 18:46:31 +0300

    Task64 code written.

---
+++ Task64/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 64: Задайте значения M и N.
+Напишите рекурсивный метод, который выведет все натуральные числа кратные 3-ём в промежутке от M до N.
+M = 1; N = 9. -> "3, 6, 9"
+M = 13; N = 20. -> "15, 18"
+*/
+
+void RecursiveMethod(int M = 1, int N = 33)
+{
+    if (M > N) return;
+    if (M%3 == 0)
+    Console.Write(M + " ");
+    M++;
+    RecursiveMethod(M, N);
+}
+RecursiveMethod();
