599479395f69c62bc84831c7de18deaf7bc25737
BogdanKorolev <bkorolevjob@gmail.com>
2022-08-26 18:46:52 +0300

    Task66 code written.

---
+++ Task66/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 66:
+Задайте значения M и N.
+Напишите рекурсивный метод,
+который найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int sum = 0;
+int M = 4;
+int N = 8;
+void RecursiveMethod(int counter = 1)
+{
+    if (counter > N-M+1) return;
+    sum = sum + (M+N);
+    counter++;
+    RecursiveMethod(counter);
+}
+RecursiveMethod();
+Console.WriteLine(sum/2);
